01b4c1a55ad5bdeaa14aabe2717e19b489c724f2
Ducphan2k <119691110+Ducphan2k@users.noreply.github.com>
2023-07-06 23:44:21 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card--header" />
+        <img
+          className="avatar"
+          src="https://th.bing.com/th/id/OIP.GNE4wD9vCiG8BNRCYx7F3gAAAA?pid=ImgDet&rs=1"
+          alt="avatar"
+        />
+        <div className="card--body">
+          <div>
+            <p className="text-header">Ruma Khan</p>
+            <p className="text-sub">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry
+            </p>
+            <button className="btn third">FOLLOW</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
